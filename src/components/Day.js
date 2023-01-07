d87c7a393063f0ac1a6dc8abf60537588e0bdb21
adityadaffaaa <adityadaffafawwaz@gmail.com>
2023-01-08 00:48:15 +0700

    Hawa Weather Apps

--- src/components/Day.js
+++ src/components/Day.js
@@ -2,24 +2,34 @@ import { React, useEffect, useState } from "react";
 
 const Day = () => {
   const [detikState, setDetikState] = useState();
+  const [menitState, setMenitState] = useState();
+  const [jamState, setJamState] = useState();
 
-  const listHari = ["Senin", "Selasa", "Rabu", "Kamis", "Jumat", "Sabtu", "Minggu"];
+  const listHari = ["Minggu", "Senin", "Selasa", "Rabu", "Kamis", "Jumat", "Sabtu"];
   const listBulan = ["Januari", "Februari", "Maret", "April", "May", "Juni", "Juli", "Agustus", "September", "Oktober", "November", "Desember"];
 
   var getHari = new Date().getDay();
   var getBulan = new Date().getMonth();
   let getTahun = new Date().getFullYear();
-  let jam = new Date().getHours();
-  let menit = new Date().getMinutes();
-  let tanggal = getHari;
+  let tanggal = new Date().getDate();
   let hari = listHari[getHari];
   let bulan = listBulan[getBulan];
   useEffect(() => {
     setInterval(() => {
       let detik = new Date().getSeconds();
+      let jam = new Date().getHours();
+      let menit = new Date().getMinutes();
       setDetikState(detik);
+      setMenitState(menit);
+      setJamState(jam);
       if (detik < 10) {
         setDetikState((detik = `0${detik}`));
+      }
+      if (menit < 10) {
+        setMenitState((menit = `0${menit}`));
+      }
+      if (jam < 10) {
+        setJamState((jam = `0${jam}`));
       }
     }, 1000);
   });
@@ -31,7 +41,7 @@ const Day = () => {
       </p>
       <span className="h-auto w-[2px] bg-text2 rounded-full"></span>
       <p>
-        {jam}.{menit}.{detikState}
+        {jamState}.{menitState}.{detikState}
       </p>
     </div>
   );
